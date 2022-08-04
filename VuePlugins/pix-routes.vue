395a7058bdaac1f83fcb4a80f3ad8cbaec6c0dbb
pixsil <57232302+pixsil@users.noreply.github.com>
2022-08-04 15:20:37 +0200

    Create pix-routes.vue

---
+++ VuePlugins/pix-routes.vue
@@ -0,0 +1,13 @@
+<template>
+</template>
+
+<script>
+export default {
+    props: {
+        routes: Object,
+    },
+    created: function () {
+        this.$setRoutes(this.routes);
+    },
+}
+</script>
